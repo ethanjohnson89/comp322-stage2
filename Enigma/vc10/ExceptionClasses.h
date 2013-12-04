19e29a001e156821720f27bea5b5fbe8f37531dc
Ethan J. Johnson <ethanjohnson89@gmail.com>
2013-12-03 19:19:59 -0500

    +AreaNotFoundException, -GameManagerException

--- Enigma/vc10/ExceptionClasses.h
+++ Enigma/vc10/ExceptionClasses.h
@@ -7,9 +7,9 @@ using std::exception;
 /************  Exception Classes  **************/
 
 class EnigmaException : public std::exception { };
-class GameManagerException : public EnigmaException { };
 class InvalidIndexException : public EnigmaException { };
-class CommandNotFoundException : public GameManagerException { };
+class CommandNotFoundException : public EnigmaException { };
+class AreaNotFoundException : public EnigmaException { };
 
 /*****************************************/
 
