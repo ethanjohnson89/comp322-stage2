27300e4c62454e47c9877f9684a46ef3442f7604
barrymillham <j.mart.x93@gmail.com>
2013-12-04 17:01:38 -0500

    Moved CommandFunction typedef to globals
    Yeah

--- Enigma/src/Command.h
+++ Enigma/src/Command.h
@@ -13,10 +13,6 @@ using std::string;
 
 class Command {
 public:
-	typedef string (*CommandFunction)(GameManager* gm, vector<string> args); //Function signature for Commanmd actions. Arguments to the command, 
-																		// as identified by the parser, are passed to the command in the vector args. 
-																		// The pointer to the GameManager allows action functions to interact with 
-																		// the game as desired by the client.
 	void setAssociatedFunction(CommandFunction *fn) {associatedFunction = fn;}
 	CommandFunction* getAssociatedFunction() {return associatedFunction;}
 	string getKey() {return key;}
